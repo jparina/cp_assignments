6f7d6bf9ca00ead01c194e46bfaae12ac0dbd4cc
Josh Parina <joshua.parina@gmail.com>
2023-02-08 14:50:33 -0600

    added pair programming solution to deafgma.py and day 4 find armstrong

--- cp_day3/deaf_grandma.py
+++ cp_day3/deaf_grandma.py
@@ -22,3 +22,50 @@ def deaf_grandma():
         second_goodbye = None
     
 deaf_grandma()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def deaf_grandma():
+#     grandma = 'HEY KID!'
+    
+#     last_response = None
+    
+#     while True:
+#         response = input(f'{grandma}')    
+#         print(response)
+#         if last_response == "GOODBYE!" and response == "GOODBYE!":
+#             print("LATER SKATER!")
+#             break
+#         elif response == "GOODBYE!" and last_response != "GOODBYE!":
+#             grandma = "LEAVING SO SOON?"
+#         elif response == '':
+#             grandma = "WHAT?!"
+#         elif response.upper() == response:
+#             grandma = "NO, NOT SINCE 1946!"
+        
+#         else:
+#             grandma = "SPEAK UP, KID!"
+#         last_response = response
+            
+# deaf_grandma()
---
+++ cp_day4/find_armstrong_numbers.py
@@ -0,0 +1,27 @@
+# finds armstrong number given an array of digits
+def armstrong_helper(array):
+    sum = 0
+    for num in  array:
+        sum += num ** len(array)
+    return sum
+    
+def find_armstrong_numbers(num):
+    ans = []
+    for i in range(num+1):
+        
+        digits = []
+        for char in str(i):
+            digits.append(int(char))
+        # print(digits)
+        armstrong = armstrong_helper(digits)
+        # print(i, armstrong)
+        if armstrong == i:
+            ans.append(i)
+    return ans
+
+# given a number n, determine if it's an armstrong number
+def useful_armstrong(n):
+    num_to_array = 
+
+# print(find_armstrong_numbers(999))
+# print(armstrong_helper([3,7,1]))
