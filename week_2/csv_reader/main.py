270c162a44401fc68d4db3052d1549da08b70744
Josh Parina <joshua.parina@gmail.com>
2023-02-13 15:37:31 -0600

    finished csv reader program

--- week_2/csv_reader/main.py
+++ week_2/csv_reader/main.py
@@ -1,2 +1,14 @@
-def function():
-    pass
+import csv
+
+def needs_a_home(animal=None):
+    animal = input("Please choose a type of animal: ").lower()
+    try:
+        with open(f'/Users/joshparina/repos/cp_assignments/week_2/csv_reader/data/{animal}.csv', mode = 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            print('These animals need a home!')
+            for row in csv_reader:
+                print(f'{row["name"].capitalize()} is a {row["age"]} year old {row["breed"]}')
+    except:
+        print(f"We dont have any {animal} here")
+    
+needs_a_home()
