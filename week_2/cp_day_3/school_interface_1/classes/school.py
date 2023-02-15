0d2a2d1e057918eec4cb6cb7c2386af775bffe83
Josh Parina <joshua.parina@gmail.com>
2023-02-15 14:16:46 -0600

    typo on self.staff and self.students

--- week_2/cp_day_3/school_interface_1/classes/school.py
+++ week_2/cp_day_3/school_interface_1/classes/school.py
@@ -4,6 +4,6 @@ from classes.staff import Staff
 class School:
     def __init__(self, name) -> None:
         self.name = name
-        self.staff = Student.all_students()
-        self.students = Staff.all_staff()
+        self.staff = Staff.all_staff()
+        self.students = Student.all_students()
         
--- week_2/cp_day_3/school_interface_1/runner.py
+++ week_2/cp_day_3/school_interface_1/runner.py
@@ -3,5 +3,3 @@ from classes.school import School
 school = School('Ridgemont High') 
 
 
-print(school.students)
-print(school.staff)
